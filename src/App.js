b681b259d5c94c6ab87914e9b10c424b703cf081
Parvane Zand <parvane.zand@live.com>
2023-08-26 18:30:46 +0330

    footer edited

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Weather city="Tehran" />
       </header>
-      <footer>
+      <footer className="App-footer">
         Coded by {""}
         <a
           target="_blank"
